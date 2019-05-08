2bd420e24ccb73907365785b9419f5c2f599f530
Sam Tyner <sctyner@iastate.edu>
2019-05-08 10:44:33 -0500

    really proud of this glossary! :smile:

--- code/generate-glossary.R
+++ code/generate-glossary.R
@@ -1,18 +1,24 @@
 # code to generate glossary
 
 library(googlesheets4)
+# change this to a regular old csv eventually and ship it with the book.
 terms <- googlesheets4::sheets_read("1i2yBb5T1Dyxm_gT4oXwjm-5DSkeNftCCdcmPjbeqHx8")
 
+terms <- terms %>%
+  mutate(source_md = ifelse(is.na(source_link), source_display,
+                            paste0("[", source_display, "](", source_link, ")")),
+         chapter = ifelse(!is.na(chapter), paste0("## ", chapter, "\n"), NA))
 
 library(glue)
 
-glossary_template <- "```{{definition, label = '{label}'}}
-**: {term}** - {definition_with_punc} *Source: {source_in_md_form}*
+glossary_template <- "{chapter}
+```{{definition, label = '{label}'}}
+**: {term}** - {definition_with_punc} *Source: {source_md}*
 ```
 
 "
 
-res <- glue_data(terms, glossary_template)
+res <- glue_data(terms, glossary_template, .na = "")
 
 res
 
@@ -26,10 +32,3 @@ Terms are given in the order in which they appear in the body of the text.
 
 
 cat(header, res, sep = "\n", file = "98-glossary.Rmd")
-
-# Don't forget to paste these lines (uncommented of course) at the top of 98-glossary.Rmd before building the book!!!
-# (APPENDIX) Appendix {-}
-
-# Glossary {#glossary}
-
-Terms are given in the order in which they appear in the body of the text.
